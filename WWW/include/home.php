8e82295b9388ae21443cad452998f6def444a071
Flipsyde <fpsyde@gmail.com>
2018-05-15 20:27:08 +0200

    Odnośniki na stronie głównej

--- WWW/include/home.php
+++ WWW/include/home.php
@@ -1,1 +1,37 @@
-Strona główna
+<div class="container">
+    <div class="row">
+        <div class="col col-xl-4">
+            <div class="card">
+                <div class="card-body bg-image">
+                    <div class="hover01 column">
+                        <div>
+                            <figure><a href="index.php?go=players"><img class="img-responsive" src="img/siatkarze.jpg"></a></figure>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col col-xl-4">
+            <div class="card">
+                <div class="card-body bg-image">
+                    <div class="hover01 column">
+                        <div>
+                            <figure><a href="index.php?go=teams"><img class="img-responsive" src="img/zespoly.jpg"></a> </figure>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col col-xl-4">
+            <div class="card">
+                <div class="card-body bg-image">
+                    <div class="hover01 column">
+                        <div>
+                            <figure><a href="index.php?go=competitions"><img class="img-responsive" src="img/rozgrywki.jpg"></a></figure>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
